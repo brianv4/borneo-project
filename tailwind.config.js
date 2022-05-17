218361456d02aa7b316fe0d2b1aa21e85d56f9e2
brianv4 <55471120+brianv4@users.noreply.github.com>
2022-05-18 03:05:50 +0700

    fix 3 gambar jadi satu sudah bisa gays, sorry aku kelelawar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    content: ["index.html"],
+    content: [
+        "index.html",
+        "webinar.html",
+        "virtual_expo.html",
+    ],
     darkMode: "class",
     theme: {
         container: {

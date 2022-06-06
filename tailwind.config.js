79145c26e2523449f247ccce9b47ee190b29a746
brianv4 <55471120+brianv4@users.noreply.github.com>
2022-06-06 20:52:56 +0700

    fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
         "index.html",
         "webinar.html",
         "virtual_expo.html",
+        "news.html",
+        "event.html",
     ],
     darkMode: "class",
     theme: {
@@ -12,9 +14,10 @@ module.exports = {
         },
         extend: {
             colors: {
-                // primary: "#14b8a6",
-                primary: "#584834",
-                secondary: "#64748b",
+                primary: "#5C2F1C",
+                secondary: "#D0844C",
+                tertiary: "#C0c6c2",
+                fourth: "#14100C",
                 dark: "#0f172a",
             },
             screens: {
